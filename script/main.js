4f4ab086c335b0a087d57afa12954f2c0311ae5c
roxsb <rox.san.bri@gmail.com>
2018-12-07 01:26:43 +0100

    1to4

---
+++ script/main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// document.querySelector('h1').innerHTML = '¡Esta página no es compatible con la versión actual de tu navegador. Por favor actualízalo a la versión más reciente.!';
+ 
+
+//Ejercicio 2
+
+// let myAdress = 'Calle Manzanares, 26';
+// myAdress ='Calle Falsa 1,2,3';
+// document.querySelector('h1').innerHTML = (myAdress);
+
+
+//Ejercicio 3
+
+// const textHello = document.querySelector('.text');
+// textHello.innerHTML = 'Hola mundo';
+
+
+//Ejercicio 4
+
+// const titleTwo = document.querySelector('h2');
+// const one = document.querySelector('.number-one');
+// const two = document.querySelector('.number-two');
+// titleTwo.innerHTML = titleTwo.innerHTML +''+ two.innerHTML;
+
+
+//Ejercicio 5
+
